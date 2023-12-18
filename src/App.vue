98ea4d0cdd8b2ea0a08c80039f235422f0792b95
dev-11 <otto@masterbranch.io>
2023-12-18 12:17:54 +0000

    initial version

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <section v-if="errored">
+      <p>We're sorry, we're not able to retrieve this information at the moment, please try back later</p>
+    </section>
+    <section v-else>
+      <div v-if="loading">
+        <div class="spinner-border text-primary" role="status">
+          <span class="sr-only">Loading...</span>
+        </div>
+      </div>
+      <div v-else class="d-flex justify-content-md-center align-items-center vh-100">
+        <div>
+          <Wordle v-bind:word="word"/>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+
+import axios from "axios";
+import Wordle from "./components/Wordle.vue";
+
+export default {
+    name: "App",
+    data() {
+        return {
+            word: "",
+            errored: false,
+            loading: true
+        };
+    },
+    created() {
+        axios.get("https://api.masterbranch.io/b/wd")
+        .then(response => (this.word = response.data.data))
+        .catch(error => {
+          console.log(error)
+          this.errored = true;
+        })
+        .finally(() => (this.loading = false));
+    },
+    components: { Wordle }
+};
+
+</script>
+
+<style>
+
+body {
+  background: #f6f6f6;
+}
+
+.spinner-border {
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  width: 50px;
+  height: 50px;
+  margin: auto;
+}
+
+.sr-only {
+  display: none;
+}
+
+</style>
---
+++ src/components/Wordle.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="letter-row">
+      <div v-bind:key="i" v-for="(char, i) in word" class="letter-box">
+        {{ char }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Wordle",
+  props: {
+    word: String
+  },
+};
+</script>
+
+<style>
+.letter-box {
+  border: 2px solid #787c7e;
+  margin: 2px;
+  font-size: 2.5rem;
+  font-weight: 700;
+  height: 3rem;
+  width: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-transform: uppercase;
+}
+
+.letter-row {
+  display: flex;
+}
+</style>
